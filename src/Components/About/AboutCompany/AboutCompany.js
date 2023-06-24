2ea24b585784a18990cfa61fb93944ece4b8302f
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-06-24 15:22:34 +0100

    about page started

--- src/Components/About/AboutCompany/AboutCompany.js
+++ src/Components/About/AboutCompany/AboutCompany.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import "../AboutCompany/AboutCompany.css";
 
 export const AboutCompany = () => {
   return (
     <>
         <section id='AboutCompany'>
             <div className='AboutCompanyContainer'>
-                <div className='aleft'></div>
-                <div className='aright'></div>
+                <div className='aleft'> <img src='img/about/about-main.jpg' /> </div>
+                <div className='aright'>
+                  <p className='title'>About Company</p>
+                  <h1> You start the engine and your adventure begins</h1>
+                  <p>
+                      Certain but she but shyness why cottage. 
+                      Guy the put instrument sir entreaties affronting. 
+                      Pretended exquisite see cordially the you.
+                       Weeks quiet do vexed or whose.
+                        Motionless if no to affronting imprudence no precaution. 
+                        My indulged as disposal strongly attended."
+                  </p>
+                  <div >
+                    <img src='/img/about/icon1.png' />
+                    <span>
+                    <h1>20</h1>
+                    <p>Car Types</p>
+                    </span>
+                  </div>
+                </div>
             </div>
         </section>
     </>
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Title } from '../Components/About/Title/Title'
+import { AboutCompany } from '../Components/About/AboutCompany/AboutCompany'
+import { PlanTrip } from '../Components/Home/PlanTrip/PlanTrip';
+import { Footer } from '../Components/General/Footer/Footer';
 
 export const About = () => {
   return (
     <>
         <Title></Title>
+        <AboutCompany></AboutCompany>
+        <PlanTrip></PlanTrip>
+        <div className='SmallBanner'>
+          <div className='Scenter'>
+                    <h3> Book a car by getting in touch with us </h3>
+                    <p> (123) 456-7869 </p>
+          </div>
+      </div>
+        <Footer></Footer>
     </>
   )
 }

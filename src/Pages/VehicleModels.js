33e8b719b65e4bfd71c8501d941aee65a6956f1f
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-06-30 18:15:02 +0100

    animation added

--- src/Pages/VehicleModels.js
+++ src/Pages/VehicleModels.js
@@ -1,23 +1,75 @@
-import React from 'react'
-import { Title } from '../Components/General/Title/Title'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { Title } from '../Components/General/Title/Title';
 import { Footer } from '../Components/General/Footer/Footer';
 import { DisplayGrid } from '../Components/VehicleModels/DisplayGrid/DisplayGrid';
 
 export const VehicleModels = () => {
+  const [titleRef, titleInView] = useInView({
+    triggerOnce: false,
+    rootMargin: '-100px 0px',
+  });
+
+  const [gridRef, gridInView] = useInView({
+    triggerOnce: false,
+    rootMargin: '-100px 0px',
+  });
+
+  const [bannerRef, bannerInView] = useInView({
+    triggerOnce: false,
+    rootMargin: '-100px 0px',
+  });
+
+  const animationVariants = {
+    hidden: {
+      opacity: 0,
+      y: 40,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1, // Adjust the duration as desired
+      },
+    },
+  };
+
   return (
     <>
-        <Title 
-          title= "Vehicle Models"
-          link = "Home / Vehicle Models"
-        ></Title>
-        <DisplayGrid></DisplayGrid>
-        <div className='SmallBanner'>
-          <div className='Scenter'>
-                    <h3> Book a car by getting in touch with us </h3>
-                    <p> (123) 456-7869 </p>
-          </div>
+      <Title
+        title="Vehicle Models"
+        link="Home / Vehicle Models"
+        ref={titleRef}
+        initial="hidden"
+        animate={titleInView ? 'visible' : ''}
+        variants={animationVariants}
+      />
+
+      <motion.div
+        className="DisplayGrid"
+        ref={gridRef}
+        initial="hidden"
+        animate={gridInView ? 'visible' : ''}
+        variants={animationVariants}
+      >
+        <DisplayGrid />
+      </motion.div>
+
+      <motion.div
+        className="SmallBanner"
+        ref={bannerRef}
+        initial="hidden"
+        animate={bannerInView ? 'visible' : ''}
+        variants={animationVariants}
+      >
+        <div className="Scenter">
+          <h3>Book a car by getting in touch with us</h3>
+          <p>(123) 456-7869</p>
         </div>
-        <Footer></Footer>
+      </motion.div>
+
+      <Footer />
     </>
-  )
-}
+  );
+};

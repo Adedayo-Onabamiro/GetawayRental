7e36919208ab9923438e08c7f5f7c66ed5346a83
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-07-04 12:59:09 +0100

    nav fix

--- src/Components/General/Navigation/Navigation.js
+++ src/Components/General/Navigation/Navigation.js
@@ -1,5 +1,134 @@
 
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+// import { Link, useLocation } from 'react-router-dom';
+// import { motion } from 'framer-motion';
+// import '../Navigation/Navigation.css';
+
+// export const Navigation = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const location = useLocation();
+
+//   const handleToggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   const handleItemClick = () => {
+//     setIsOpen(false);
+//   };
+
+//   const handleOutsideClick = (event) => {
+//     if (event.target.closest('.NavMenuContainer')) {
+//       return;
+//     }
+//     setIsOpen(false);
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener('click', handleOutsideClick);
+//     return () => {
+//       window.removeEventListener('click', handleOutsideClick);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     setIsOpen(false); // Close the menu when location changes
+//   }, [location]);
+
+//   const containerVariants = {
+//     hidden: { opacity: 0, y: '-100%' },
+//     visible: { opacity: 1, y: 0 },
+//   };
+
+//   const logoVariants = {
+//     hidden: { opacity: 0, y: '-20px' },
+//     visible: { opacity: 1, y: 0 },
+//   };
+
+//   const menuVariants = {
+//     hidden: { opacity: 0, y: '-20px' },
+//     visible: { opacity: 1, y: 0 },
+//   };
+
+//   const signRegisterVariants = {
+//     hidden: { opacity: 0, y: '20px' },
+//     visible: { opacity: 1, y: 0 },
+//   };
+
+//   return (
+//     <>
+//       <section className="Header" id="Header">
+//         <motion.div
+//           className="TopNavContainer"
+//           initial="hidden"
+//           animate="visible"
+//           variants={containerVariants}
+//           transition={{ type: 'tween', duration: 0.8 }}
+//         >
+//           <motion.div
+//             className="LogoBox"
+//             variants={logoVariants}
+//             transition={{ type: 'tween', duration: 0.8, delay: 0.3 }}
+//           >
+//             <img src="../img/logo/logo.png" alt="logo" />
+//           </motion.div>
+//           <motion.nav
+//             className="NavMenuContainer"
+//             variants={menuVariants}
+//             transition={{ type: 'tween', duration: 0.8, delay: 0.5 }}
+//           >
+//             <div className="hamburger" onClick={handleToggleMenu}>
+//               <div className="bar"></div>
+//             </div>
+//             <ul className={`NavMenuUl ${isOpen ? 'active' : ''}`}>
+//               <li className="NavMenuLi" onClick={handleItemClick}>
+//                 <Link to="/" onClick={handleItemClick}>
+//                   Home
+//                 </Link>
+//               </li>
+//               <li className="NavMenuLi" onClick={handleItemClick}>
+//                 <Link to="/about" onClick={handleItemClick}>
+//                   About
+//                 </Link>
+//               </li>
+//               <li className="NavMenuLi" onClick={handleItemClick}>
+//                 <Link to="/vehiclemodels" onClick={handleItemClick}>
+//                   Vehicle Models
+//                 </Link>
+//               </li>
+//               <li className="NavMenuLi" onClick={handleItemClick}>
+//                 <Link to="/testimonialPage" onClick={handleItemClick}>
+//                   Testimonials
+//                 </Link>
+//               </li>
+//               <li className="NavMenuLi" onClick={handleItemClick}>
+//                 <Link to="/ourteam" onClick={handleItemClick}>
+//                   Our Team
+//                 </Link>
+//               </li>
+//               <li className="NavMenuLi" onClick={handleItemClick}>
+//                 <Link to="/contact" onClick={handleItemClick}>
+//                   Contact
+//                 </Link>
+//               </li>
+//             </ul>
+//           </motion.nav>
+
+//           <motion.div
+//             className="SignRegister"
+//             variants={signRegisterVariants}
+//             transition={{ type: 'tween', duration: 0.8, delay: 0.7 }}
+//           >
+//             <p>Sign In</p>
+//             <button style={{ fontSize: 'inherit' }}>Register</button>
+//           </motion.div>
+//         </motion.div>
+//       </section>
+//     </>
+//   );
+// };
+
+
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import '../Navigation/Navigation.css';
@@ -7,6 +136,7 @@ import '../Navigation/Navigation.css';
 export const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
+  const navbarRef = useRef(null);
 
   const handleToggleMenu = () => {
     setIsOpen(!isOpen);
@@ -23,10 +153,21 @@ export const Navigation = () => {
     setIsOpen(false);
   };
 
+  const handleScroll = () => {
+    const navbarHeight = navbarRef.current.offsetHeight;
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollPosition > navbarHeight * 0.6) {
+      setIsOpen(false);
+    }
+  };
+
   useEffect(() => {
     window.addEventListener('click', handleOutsideClick);
+    window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('click', handleOutsideClick);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
@@ -56,7 +197,7 @@ export const Navigation = () => {
 
   return (
     <>
-      <section className="Header" id="Header">
+      <section className="Header" id="Header" ref={navbarRef}>
         <motion.div
           className="TopNavContainer"
           initial="hidden"
